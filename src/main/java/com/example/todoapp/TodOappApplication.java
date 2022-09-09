112ef9aa4ae02d42af93b12e80c2247f9a254380
ivivix90 <wiszenkoigor@gmail.com>
2022-09-09 18:58:12 +0200

    initial commit

---
+++ src/main/java/com/example/todoapp/TodOappApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodOappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodOappApplication.class, args);
+    }
+
+}
